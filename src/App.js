d8adf6f1dcd6581083cf90478472d4258991f225
Shoryu Das-Zaman <58425525+ShoryuDZ@users.noreply.github.com>
2021-07-03 19:46:09 +1000

    App Setup :smile:
    Setting up the initial all using create-react-app CLI tool :+1:

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import swlogo from './media/star_wars.png';
+
+function App() {
+  return (
+    <div className="sw-app">
+      <img className="center-logo" src={swlogo} alt="Star Wars Logo"></img>
+    </div>
+  );
+}
+
+export default App;
